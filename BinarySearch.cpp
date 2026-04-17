b6c0067691003ae72335b12039550682b924d294
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-04-17 14:37:52 +0700

    void

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -6,3 +6,13 @@ using namespace std;
 int element[10];
 int npanjang;
 int x;
+
+//void
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10) : ";
+        cin >> npanjang;
+    }
+}
